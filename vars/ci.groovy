25db3e3009e33c2dec1877f14155ae3cd4ef8e97
mudur <mudurukollaramsai335@gmail.com>
2024-05-04 08:12:58 +0530

    scripted pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,13 +1,12 @@
 def call() {
     node('workstation') {
         stage('Checkout') {
-
             cleanWs()
-
             git branch: 'main', url: "https://github.com/Ramsai33/cart.git"
-
             sh 'env'
-
-            }
+        }
+        stage('Build') {
+            echo 'Hello'
+        }
     }
 }
