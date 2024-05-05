32ab85b2beef4936cc12bd986c655679a2152aaa
mudur <mudurukollaramsai335@gmail.com>
2024-05-05 06:17:36 +0530

    declarative pipeline

--- vars/common.groovy
+++ vars/common.groovy
@@ -10,7 +10,7 @@ def compile() {
 def artifactspush() {
     sh "echo ${TAG_NAME} >VERSION"
     if (app_lang == "nodejs") {
-        sh "zip -r cart-${TAG_NAME}.zip node_modules server.js VERSION"
+        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
     }
     NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
