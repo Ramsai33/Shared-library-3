2f35a6ee53dd2b576c17e6affcfa48f729712788
mudur <mudurukollaramsai335@gmail.com>
2024-05-05 06:55:14 +0530

    declarative pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,4 +1,10 @@
 def call() {
+    if(!env.TAG_NAME) {
+        env.PUSH_CODE = "FALSE"
+    }
+    else {
+        env.PUSH_CODE = "TRUE"
+    }
     node('workstation') {
         stage('Checkout') {
             cleanWs()
@@ -14,8 +20,10 @@ def call() {
         stage('Qualitycheck') {
             echo 'Hello'
         }
+        if(env.PUSH_CODE == "TRUE") {
         stage('Placing artifacts in centrilise Place') {
             common.artifactspush()
+           }
         }
     }
 }
